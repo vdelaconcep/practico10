62c01854dfd17a879a84617fec3927e5131285e0
vdelaconcep <vdelaconcep@gmail.com>
2025-05-13 12:47:40 -0300

    Agregados checkboxes

--- public/validaciones.js
+++ public/validaciones.js
@@ -4,7 +4,11 @@ const formulario = document.querySelector('form');
 const inputNombre = document.getElementById('nombre');
 const inputEdad = document.getElementById('edad');
 const btnEnviar = document.getElementById('btn-enviar');
+const btnEliminar = document.getElementById('btn-eliminar');
 
+
+
+// Función para validar formulario (desde el front)
 const validacion = () => {
     inputNombre.addEventListener('input', () => {
         inputNombre.setCustomValidity('');
@@ -35,12 +39,34 @@ const validacion = () => {
     }
 }
 
-// Al Enviar el formulario
-btnEnviar.addEventListener('click', (event) => {
-    const datosValidos = validacion();
+// Validación "front" del formulario
+if (btnEnviar) {
+    btnEnviar.addEventListener('click', () => {
+        const datosValidos = validacion();
 
-    if (datosValidos) {
-        formulario.submit();
-    };
+        if (datosValidos) {
+            formulario.submit();
+        };
+    });
+}
 
-});
+// Seleccionar y eliminar registros
+if (btnEliminar) {
+    btnEliminar.addEventListener('click', () => {
+        const listaAEliminar = document.querySelectorAll('input[type="checkbox"]:checked');
+        const cantidadAEliminar = listaAEliminar.length;
+        if (cantidadAEliminar > 0) {
+            let confirmacion = false;
+            if (cantidadAEliminar === 1) {
+                confirmacion = confirm('¿Desea eliminar el registro seleccionado?')
+            } else {
+                confirmacion = confirm(`¿Desea eliminar los ${cantidadAEliminar} registros seleccionados?`)
+            }
+            if (confirmacion) {
+                console.log('Se elimina/n el/los registro/s')
+            }
+        } else {
+            alert ('No se han seleccionado registros')
+        }
+    });
+}
